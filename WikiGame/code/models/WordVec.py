a2cc235fd8afed4b5d26de597eb03dc0af923f45
Tilo Reneau-Cardoso <tiloreneau@gmail.com>
2023-04-24 18:54:10 -0700

    Improved error handleing

--- WikiGame/code/models/WordVec.py
+++ WikiGame/code/models/WordVec.py
@@ -1,5 +1,5 @@
 from scipy import spatial
-
+import nltk
 
 class WordVec:
     def __init__(self, word_vectors):
@@ -8,16 +8,31 @@ class WordVec:
 
         :param word_vectors: A dictionary of word vectors.
         """
+        nltk.download('stopwords')
+        from nltk.corpus import stopwords
         self.word_vecs = word_vectors
+        self.stop_words = set(stopwords.words('english'))
+        
+    def _split_and_clean(self, document):
+        ret = []
+        for word in document.split():
+            
+            if word not in self.word_vecs:
+                word = word.lower()
+            if word not in self.word_vecs:
+                return []
+            if word in self.word_vecs and word not in self.stop_words:
+                ret.append(word)
+        return ret
 
     def _find_average_word_vec(self, word_list):
         word_vecs = []
         for word in set(word_list):
-            if word in self.word_vecs:
-                word_vecs.append(self.word_vecs[word])
+            if word not in self.word_vecs:
+                raise ValueError("Text contains out of vocab words")
+            
+            word_vecs.append(self.word_vecs[word])
 
-        if len(word_vecs) == 0:
-            raise ValueError("Text contains no words in vocabulary")
         return sum(word_vecs)/len(word_vecs)
 
     def get_closest(self, documents, comparison_document, n):
@@ -37,14 +52,25 @@ class WordVec:
         """
         def sim(vec1, vec2):
             return 1 - spatial.distance.cosine(vec1, vec2)
-
-        comparison_vec = self._find_average_word_vec(comparison_document.split())
-
-        doc_vec = {}
+        
+        # Compute comparison vector
+        comp_words_clean = self._split_and_clean(comparison_document)
+        if len(comp_words_clean) == 0:
+            raise ValueError("Comparison document only contains out of vocab words")
+        comparison_vec = self._find_average_word_vec(comp_words_clean)
+        
+        # Compute document vectors
+        doc_vecs = {}
         for doc in documents:
-            doc_word_list = doc.split()
-            doc_vec[doc] = self._find_average_word_vec(doc_word_list)
-        #print(doc_vec)
+            clean_split_doc = self._split_and_clean(doc)
+            if len(clean_split_doc) > 0:
+                doc_vecs[doc] = self._find_average_word_vec(clean_split_doc)
+    
+        if len(doc_vecs) == 0:
+            raise ValueError("No document vectors could be computed")
+            
+        
+        in_vocab_doc = doc_vecs.keys()
 
-        return sorted(documents, key=lambda doc: sim(doc_vec[doc], comparison_vec), reverse=True)[:n]
+        return sorted(in_vocab_doc, key=lambda doc: sim(doc_vecs[doc], comparison_vec), reverse=True)[:n]
 
