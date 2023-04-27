e99835b48e7754302afe93fb969530999a954026
Tilo Reneau-Cardoso <tiloreneau@gmail.com>
2023-04-26 21:48:08 -0700

    refactored code

--- WikiGame/src/algorithms/Greedy.py
+++ WikiGame/src/algorithms/Greedy.py
@@ -10,6 +10,10 @@ import os
 class Greedy(AlgorithmBase):
     def __init__(self, origin, destination, model):
         super().__init__(origin, destination, model)
+        self.destination_page = get_page_contents(destination)
+        # Blacklist articles like 'Category:Articles lacking reliable references from December 2009'
+        self.blacklist = ['Category:','Wikipedia:']
+        self.seen = set()
 
         if model == "WordVec":
             path = "data/glove.pickle"
@@ -22,8 +26,29 @@ class Greedy(AlgorithmBase):
             raise ValueError("Invalid model. Please enter a valid model.")
 
     def play(self, path):
+        
+        
+        def remove_blacklisted(hyperlinks):
+            return [h for h in hyperlinks if not any(phrase in h for phrase in self.blacklist)]
+        
+        # avoid cycles - find the first hyperlink that we haven't seen before
+        def get_next_page(hyperlinks):
+            hyperlinks = remove_blacklisted(hyperlinks)
+            closest_n = self.model.get_closest(hyperlinks, self.destination + " "+ self.destination_page , 50)
+            print(closest_n[:10])
+            for candidate_and_sim in closest_n:
+                cand, sim = candidate_and_sim
+                
+
+                
+                if cand not in self.seen and valid_link(cand):
+                    if self.model.count_vectorizable_documents(hyperlinks) > 0:
+                        self.seen.add(cand)
+                        return cand
+            return None
+                
+        
         cur = self.origin
-        seen = set()
 
         while True:
             print()
@@ -39,34 +64,13 @@ class Greedy(AlgorithmBase):
             if len(path) > self.MAX_LINKS:
                 print("Max links reached")
                 return path
-
-            # find the top n closest hyperlinks
-            closest_n = self.model.get_closest(hyperlinks, self.destination, 50)
-
-            print(closest_n[:10])
             
-    
-            closest = None
-            # avoid cycles - find the first hyperlink that we haven't seen before
-            for candidate in closest_n:
-                if candidate not in seen and valid_link(candidate):
-                    hyperlinks = find_hyperlinks(candidate)
-                    if self.model.count_vectorizable_documents(hyperlinks) > 0:
-                        closest = candidate
-                        break
-                    else:
-                        seen.add(candidate)
-                    
-            if closest is None:
-                raise ValueError("No unseen links could be found")
-                
-                
-            seen.add(closest)
-            path.append(closest)
-            print(seen)
-
-
-            cur = closest
+            
+            next_page = get_next_page(hyperlinks)
+            if next_page is None:
+                raise RuntimeError("Next page could not be found")
+            path.append(next_page)
+            cur = next_page
 
 
 ########################################################################################################################
