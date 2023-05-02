b70085a45b6747a5b6308af06ec908ed05dbb406
Tilo Reneau-Cardoso <tiloreneau@gmail.com>
2023-05-01 19:53:52 -0700

    removed nltk as a dependency

--- WikiGame/src/models/WordVec.py
+++ WikiGame/src/models/WordVec.py
@@ -1,6 +1,4 @@
 from scipy import spatial
-import nltk
-
 
 class WordVec:
     def __init__(self, word_vectors):
@@ -10,10 +8,8 @@ class WordVec:
         :param word_vectors: A dictionary of word vectors.
         """
 
-        nltk.download('stopwords')
-        # from nltk.corpus import stopwords
         self.word_vecs = word_vectors
-        self.stop_words = set()  # set(stopwords.words('english'))
+        self.stop_words = {'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"}  # set(stopwords.words('english'))
 
     def _split_and_clean(self, document, exclude_documents_with_OOV_words=True):
         ret = []
@@ -28,7 +24,7 @@ class WordVec:
                     return []
         
             # filter stop words
-            if word not in self.stop_words and word in self.word_vecs:
+            if word.lower() not in self.stop_words and word in self.word_vecs:
                 ret.append(word)
         return ret
 
